8bcbd38287cfa603a8f28f8e7b79abe327cedea7
jiangdonghua <861252878@q.com>
2018-01-10 10:26:18 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+import 'element-ui/lib/theme-chalk/index.css'
+// import locale from 'element-ui/lib/locale/lang/en'
+import '@/styles/index.scss' // global css
+import * as filters from './filters'
+import App from './App'
+import router from './router'
+import store from './store'
+import '@/icons' // icon
+import fullCalendar from 'vue-fullcalendar'
+import '@/permission' // 权限
+import '@/mock'
+
+Vue.use(ElementUI)
+Vue.component('full-calendar', fullCalendar)
+// register global utility filters.
+Object.keys(filters).forEach(key=>{
+  Vue.filter(key,filters[key])
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
